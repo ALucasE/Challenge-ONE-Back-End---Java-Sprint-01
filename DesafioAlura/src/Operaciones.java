d7f1821faf75fa53d049f2e082c40a19919cd3b7
Lucas <alucase@gmail.com>
2023-03-21 10:49:21 -0300

    Se agrega Temperatura

--- DesafioAlura/src/Operaciones.java
+++ DesafioAlura/src/Operaciones.java
@@ -1,7 +1,11 @@
 
+import java.text.DecimalFormat;
+
 import javax.swing.JOptionPane;
 
 public class Operaciones{
+		private static double resultado;
+
 		//MENU DE DIVISAS	
 		public static void abrirMenuDivisas(Divisas moneda1,Divisas moneda2,Divisas moneda3,Divisas moneda4,Divisas moneda5) {	
 			boolean continuar = true;
@@ -56,4 +60,42 @@ public class Operaciones{
 				}
 			}		
 		}
+		
+		public static void abrirMenuTemperatura() {
+			boolean continuar = true;
+			while(continuar) {
+				//Menu de opciones
+				String[] options = {"Celcius a Fahrenheit", "Fahrenheit a Celcius"};
+				int option= JOptionPane.showOptionDialog(null, "Seleccione una opcion:", "Conversor", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, options, options[0]);
+				//Input
+				double input = 0;
+				boolean bandera=true;
+				while (bandera) {
+					try {
+						input=Double.parseDouble(JOptionPane.showInputDialog("Ingrese el valor a convertir"));
+						bandera=false;
+					}catch (NumberFormatException e) {
+						JOptionPane.showMessageDialog(null, "Valor no valido - Internte de nuevo");
+					}				
+				}
+				
+				//operaciones de cambio de temperatura
+				DecimalFormat formatearTemperatura = new DecimalFormat("#.##");
+				if (option==0) {
+					resultado=(input*9/5)+32;
+					JOptionPane.showMessageDialog(null, input+"º Celcius son: "+formatearTemperatura.format(resultado)+"º Fahrenheit");
+				}else if(option==1){
+					resultado=(input-32)*5/9;
+					JOptionPane.showMessageDialog(null, input+"º Fahrenheit son: "+formatearTemperatura.format(resultado)+"º Celcius");
+				}
+				
+				//Menu continuar
+				int seguir=JOptionPane.showConfirmDialog(null,"¿Desea continuar?","Confirmar",JOptionPane.YES_NO_CANCEL_OPTION);
+				if(seguir==JOptionPane.NO_OPTION ||seguir==JOptionPane.CANCEL_OPTION) {
+					continuar = false;
+					JOptionPane.showMessageDialog(null, "Cerrando programa");
+				}
+			}
+		}
 }
+		
--- DesafioAlura/src/Principal.java
+++ DesafioAlura/src/Principal.java
@@ -16,22 +16,22 @@ public class Principal {
 		Divisas libra = new Divisas("Libra", "GBP", "Libras", 0.0041,"Pesos argentinos a Libras", "Libras a Pesos argentinos");
 		Divisas yen = new Divisas("Yen", "JPY", "Yenes", 0.66,"Pesos argentinos a Yenes", "Yenes a Pesos argentinos");
 		Divisas won = new Divisas("Won", "KRW", "Wones", 6.48,"Pesos argentinos a Wones", "Wones a Pesos argentinos");
-		int x=0;
+		int x=abrirMenuPrincipal();
+		/*
 		try {
 			x = abrirMenuPrincipal();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+		*/
 		if (x==2) {
 			JOptionPane.showMessageDialog(null, "Cerrando programa");
 			System.exit(0);
 		}else if(x==0) {
-			Operaciones.abrirMenuDivisas(dolar,euro,libra,yen,won);
-			//abrirMenuDivisas(dolar,euro,libra,yen,won);			
+			Operaciones.abrirMenuDivisas(dolar,euro,libra,yen,won);			
 		}else if(x==1) {
-			//AQUI EL CONVERSOR DE TEMPERATURA
+			Operaciones.abrirMenuTemperatura();
 		}
 	}
 
